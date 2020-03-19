d2f6e248ef48f7ad1ef51ca70723ba9f2c2178de
Sandra Luengo <sandra.luengo@evoluciona.com>
2020-03-19 19:38:44 +0100

    navigation styled ready

--- src/components/TableContainer/Pagination/Pagination.js
+++ src/components/TableContainer/Pagination/Pagination.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Pagination = ({ className }) => {
-  return <div>Pagination</div>;
+  return (
+    <div className={className}>
+      <div className='numbers'>1</div>
+      <div className='numbers'>2</div>
+      <div className='numbers'>3</div>
+      <div className='numbers'>4</div>
+      <div className='numbers'>></div>
+    </div>
+  );
 };
 
 export default Pagination;
--- src/components/TableContainer/Pagination/Pagination.styled.js
+++ src/components/TableContainer/Pagination/Pagination.styled.js
@@ -1,4 +1,35 @@
 import styled from "styled-components";
 import Pagination from "./Pagination";
 
-export default styled(Pagination).attrs({})``;
+export default styled(Pagination).attrs({})`
+  background-color: #00bc8c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  border-radius: 4px;
+  height: 8vh;
+  margin: 3% 0;
+  .numbers {
+    width: 20%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 1.2rem;
+    font-weight: bold;
+    transition: background-color 0.3s ease-in-out;
+    &:first-child {
+      background-color: #00dba3;
+      border-radius: 4px 0 0 4px;
+    }
+    &:last-child {
+      border-radius: 0 4px 4px 0;
+    }
+    &:hover {
+      background-color: #00dba3;
+      cursor: pointer;
+    }
+  }
+`;
